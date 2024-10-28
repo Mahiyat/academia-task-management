acba2901aaeffb93a835b23db9f0153698a49258
abrarTUF <jucse29.398@gmail.com>
2024-10-29 00:35:55 +0600

    add teacher model

---
+++ server/src/models/Teacher.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const TeacherSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone_number: {
+    type: String
+  },
+  designation: {
+    type: String
+  },
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course'
+    }
+  ],
+  role: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Role',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Teacher', TeacherSchema);
