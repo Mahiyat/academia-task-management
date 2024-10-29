407a4b81ef5daa70a9400c853d2ecda1aedb0307
Adnan <adnan.jucse@gmail.com>
2024-10-29 22:24:39 +0600

    Add model,controller,servic and route for course

--- server/src/services/courseServices.js
+++ server/src/services/courseServices.js
@@ -1,16 +1,38 @@
-import Course from '../models/Course.js';
+import Course from "../models/Course.js";
 
 const createNewCourse = async (courseData) => {
   const course = new Course(courseData);
 
   return await course.save();
 };
-
+ 
+// Get all courses
 const getAllCourses = async () => {
-  return await Course.find();
+  return await Course.find().populate('courseTeachers').populate('semester');
+};
+
+// Get a specific course by ID
+const getCourseById = async (courseId) => {
+  return await Course.findById(courseId).populate('courseTeachers').populate('semester');
+};
+
+// Update a course by ID
+const updateCourse = async (courseId, updatedData) => {
+  return await Course.findByIdAndUpdate(courseId, updatedData, { new: true })
+    .populate('courseTeachers')
+    .populate('semester');
+};
+
+// Delete a course by ID
+const deleteCourse = async (courseId) => {
+  return await Course.findByIdAndDelete(courseId);
 };
 
 export default {
   createNewCourse,
-  getAllCourses
+  getAllCourses,
+  getCourseById,
+  updateCourse,
+  deleteCourse,
 };
+
