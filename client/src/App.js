6bdfc00954fc213889a156e901ed631a62019b98
Soadanam8@gmail.com <Soadanam8@gmail.com>
2024-11-09 03:11:00 +0600

    add soad-messagingsystem

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+<<<<<<< HEAD
 import SemesterList from './components/SemesterList';
 import SemesterDetails from './components/SemesterDetails';
 import TeacherDashboard from './components/TeacherDashboard';
 import SemesterProgressTracker from './components/semesterProgressTracking/SemesterProgressTracker';
 import ExamCommitteeSelection from './components/ExamCommitteeSelection';
+=======
+
+
+
+import SendMessage from './Components/SendMessage';
+>>>>>>> 47a2ce6 (add soad-messagingsystem)
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SemesterList />} />
-        <Route path="/semester/:id" element={<SemesterDetails />} />
+        <Route path="/" element={<SendMessage />} />
+      
       </Routes>
     </Router>
   );
