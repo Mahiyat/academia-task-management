119a12e133438181168418ca1a7175b058236fd2
cryomancer148 <taufiqislam15@gmail.com>
2024-11-08 22:51:38 +0600

    Added more tests
    Resolve conflict

--- server/src/routes/reportGenerationRoutes.js
+++ server/src/routes/reportGenerationRoutes.js
@@ -1,11 +1,21 @@
-// routes/reportRoutes.js
 import express from 'express';
 import { getCourseReport } from '../controllers/reportGenerationController.js';
 
 // eslint-disable-next-line new-cap
 const reportGenerationRoutes = express.Router();
 
-// Route for generating a report for a specific course
+/**
+ * @summary GET route for generating a consolidated report for all courses taught 
+ * by a specific teacher.
+ * @param {string} teacherId.path.required - The ID of the teacher whose course 
+ * report needs to be generated.
+ * @returns {object} 200 - Returns a consolidated report object 
+ * @returns {object} 200 - If the teacher has no courses, returns a message indicating 
+ * no courses found.
+ * @returns {Error} 404 - Teacher not found
+ * @returns {Error} 500 - Error Message
+ */
+
 reportGenerationRoutes.get('/:teacherId', getCourseReport);
 
 export { reportGenerationRoutes };
