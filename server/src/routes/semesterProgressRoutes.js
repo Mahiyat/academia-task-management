dc21b866bb6815581f2e1deef367f76c9c106142
Adnan <adnan.jucse@gmail.com>
2024-11-13 23:27:59 +0600

    UI imrproved

--- server/src/routes/semesterProgressRoutes.js
+++ server/src/routes/semesterProgressRoutes.js
@@ -1,9 +1,23 @@
 import express from "express";
 import { getSemesterProgress } from "../controllers/semesterProgressController.js";
 
+// eslint-disable-next-line new-cap
 const progressRoutes = express.Router();
 
-// Route to get semester progress
+/**
+ * @module progressRoutes
+ * @description Defines routes for retrieving semester progress data.
+ */
+
+/**
+ * GET /:semesterId
+ * @summary Retrieves progress data for a specified semester.
+ * @function
+ * @memberof module:progressRoutes
+ * @param {string} semesterId - Unique identifier for the semester to fetch progress data.
+ * @returns {object} 200 - An object containing the semester's progress details.
+ * @returns {Error} 500 - Server error.
+ */
 progressRoutes.get('/:semesterId', getSemesterProgress);
 
-export { progressRoutes } ;
+export { progressRoutes };
